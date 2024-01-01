ea330dffa72baf778ccdd87d55b97d094e0921ea
David Jones <DAVIDJ@GSFC.org>
2023-12-31 21:14:45 -0500

    fix uggettext references

--- audit_log/models/managers.py
+++ audit_log/models/managers.py
@@ -4,7 +4,7 @@ import copy
 import datetime
 from functools import partial
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
 from audit_log.models.fields import LastUserField
